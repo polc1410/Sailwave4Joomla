0bf04435ba44cccfaaddc3c5ba45775cc6efac2e
cpolwart <cpolwart@ThinServer>
2015-03-11 15:22:13 +0000

    Add javascript handler

--- Button/sailwave.php
+++ Button/sailwave.php
@@ -8,9 +8,16 @@ defined('_JEXEC') or die;
 class plgButtonSailwave extends JPlugin {
     
     function onDisplay($name) {
+        $doc = JFactory::getDocument();
+        $jsCode = "
+                function showAlert(message) {
+                    alert(message);
+                }
+            ";
+        $doc->addScriptDeclaration($jsCode);
         $button = new JObject();
         $button->set('text','Sailwave');
-        $button->set('onclick', 'alert(\'Sailwave Pressed\');');
+        $button->set('onclick', 'showAlert(\'Sailwave Pressed\');');
         $button->set('name', 'Sailwave');
         return $button;
     }
