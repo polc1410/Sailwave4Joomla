e288936564a3fdd44b69e19ce4015e8115835d3f
cpolwart <cpolwart@ThinServer>
2015-03-11 15:56:49 +0000

    Work on javascript modal window

--- Button/sailwave.php
+++ Button/sailwave.php
@@ -10,15 +10,28 @@ class plgButtonSailwave extends JPlugin {
     function onDisplay($name) {
         $doc = JFactory::getDocument();
         $jsCode = "
-                function showAlert(message) {
-                    alert(message);
+                function insertText(nameOfEditor) {
+                    jInsertEditorText('Hello World, I am doing just fine', nameOfEditor);
                 }
+               
+            function SailwaveCallback( editor, result){
+                if( result  ) { 
+                    alert(result);
+                    insertText('CALUM');
+                }
+            }
+       
             ";
         $doc->addScriptDeclaration($jsCode);
+        $link = '../plugins/editors-xtd/sailwave/dialog.php?ih_name='.$name;
+        JHTML::_('behavior.modal');
         $button = new JObject();
+        $button->set('modal', true);
         $button->set('text','Sailwave');
-        $button->set('onclick', 'showAlert(\'Sailwave Pressed\');');
+        //$button->set('onclick', 'insertText(\''.$name.'\');');
         $button->set('name', 'Sailwave');
+        $button->set('link', $link);
+        $button->set('options', "{handler: 'iframe', size: {x: 570, y: 400}}");
         return $button;
     }
 }
