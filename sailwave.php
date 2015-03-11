875e3afcc47372276aec3673e62de01997a65435
cpolwart <cpolwart@ThinServer>
2015-03-11 12:02:04 +0000

    Version 0.1 - starting point replaces the text {sailwave: 01012015} with Sailwave Results Here in an article

--- sailwave.php
+++ sailwave.php
@@ -6,7 +6,6 @@ defined('_JEXEC') or die;
 
 class plgContentSailwave extends JPlugin {
     
-
     public function onContentPrepare($context, &$row, &$params, $page = 0) {
         $row->text = str_replace("{sailwave: 01012015}","Sailwave Results Here",$row->text);
     }
