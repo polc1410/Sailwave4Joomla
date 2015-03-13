fbed936d19fb92d28211c49815fd8a7ce498969b
cpolwart <cpolwart@ThinServer>
2015-03-13 16:15:15 +0000

    Identify if home menu item

--- mod_sailwavemenu/tmpl/default.php
+++ mod_sailwavemenu/tmpl/default.php
@@ -9,9 +9,10 @@
 defined('_JEXEC') or die;
 
 // Note. It is important to remove spaces between elements.
-$path = realpath(JPATH_SITE . DIRECTORY_SEPARATOR . "results");  //TODO
-
-$objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST);
+//THIS STUFF BELONGS IN HELPER
+$sailwavepath = realpath(JPATH_SITE . DIRECTORY_SEPARATOR . "results");  //TODO
+$pathdepth = substr_count ( $sailwavepath , DIRECTORY_SEPARATOR );
+$objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($sailwavepath), RecursiveIteratorIterator::SELF_FIRST);
 echo "<ul>";
 $counter = 0;
 $incrementer = 0;
@@ -25,7 +26,6 @@ foreach($objects as $name => $object){
     if (is_dir($name))
         {
         if ($alias!=".." && $alias !="." && $alias !="") {
-            echo "<li>".$counter."|D|". $alias . "</li>";
             //TODO remove empty folders?
             $menuItem->    id = $counter;
             $menuItem->    menutype = "";
@@ -34,15 +34,21 @@ foreach($objects as $name => $object){
             $menuItem->    note = "";
             $menuItem->    route = $name;
             $menuItem->    link = $name; //TODO
-            $menuItem->    type = "component"; //TODO
-            $menuItem->    level = ""; //TODO
+            $menuItem->    type = "separator"; //TODO
+            $menuItem->    level = substr_count ( $name , DIRECTORY_SEPARATOR )-$pathdepth ; 
+            //$menuItem->title = $menuItem->alias . "(".$menuItem->level.")";
             $menuItem->    language = "*";
             $menuItem->    browserNav = 0;
             $menuItem->    access = 1;
-            $menuItem->    home = 0;
+            if($name == $sailwavepath) {
+                $menuItem->    home = 1;
+            } else {
+                $menuItem -> home = 0;
+            }
             $menuItem->    img = "";
             $menuItem->    template_style_id = 0; 
             $menuItem->    component_id = "";
+            $parent = dirname ( $name );
             $menuItem->    parent_id = 1 ;//TODO
             $menuItem->    component = "com_sailwave"; 
             $menuItem->    deeper = "";
@@ -59,7 +65,6 @@ foreach($objects as $name => $object){
     } 
     else {
         if ($filetype=="html"){
-            echo "<li>" .$counter."|F|". $alias."</li>";
             $menuItem->id = $counter;
             $menuItem->    menutype = "";
             $menuItem->    title = $alias;
@@ -67,8 +72,8 @@ foreach($objects as $name => $object){
             $menuItem->    note = "";
             $menuItem->    route = $name;
             $menuItem->    link = $name; //TODO
-            $menuItem->    type = "component"; //TODO
-            $menuItem->    level = ""; //TODO
+            $menuItem->    type = "url"; //TODO
+            $menuItem->    level = substr_count ( $name , DIRECTORY_SEPARATOR )-$pathdepth; 
             $menuItem->    language = "*";
             $menuItem->    browserNav = 0;
             $menuItem->    access = 1;
@@ -91,12 +96,104 @@ foreach($objects as $name => $object){
         }
     } 
     if ($validEntry){
-        $items [ $incrementer ] = $menuItem;
+        $list [ $incrementer ] = $menuItem;
         $incrementer ++;
         }
     }
 echo "</ul>";
-print_r($items);
+//print_r($items);
 ?>
 
+<?php // The menu class is deprecated. Use nav instead. ?>
+<ul class="nav menu<?php echo $class_sfx;?>"<?php
+$tag = '';
+
+if ($params->get('tag_id') != null)
+{
+$tag = $params->get('tag_id') . '';
+echo ' id="' . $tag . '"';
+}
+?>>
+<?php
+foreach ($list as $i => &$item)
+{
+$class = 'item-' . $item->id;
+
+if (($item->id == $active_id) OR ($item->type == 'alias' AND $item->params->get('aliasoptions') == $active_id))
+{
+$class .= ' current';
+}
+
+if (in_array($item->id, $path))
+{
+$class .= ' active';
+}
+elseif ($item->type == 'alias')
+{
+$aliasToId = $item->params->get('aliasoptions');
+
+if (count($path) > 0 && $aliasToId == $path[count($path) - 1])
+{
+$class .= ' active';
+}
+elseif (in_array($aliasToId, $path))
+{
+$class .= ' alias-parent-active';
+}
+}
+
+if ($item->type == 'separator')
+{
+$class .= ' divider';
+}
+
+if ($item->deeper)
+{
+$class .= ' deeper';
+}
+
+if ($item->parent)
+{
+$class .= ' parent';
+}
+
+if (!empty($class))
+{
+$class = ' class="' . trim($class) . '"';
+}
+
+echo '<li' . $class . '>';
+
+// Render the menu item.
+switch ($item->type) :
+case 'separator':
+case 'url':
+case 'component':
+case 'heading':
+require JModuleHelper::getLayoutPath('mod_menu', 'default_' . $item->type); //todo
+break;
+
+default:
+require JModuleHelper::getLayoutPath('mod_menu', 'default_url'); //todo
+break;
+endswitch;
+
+// The next item is deeper.
+if ($item->deeper)
+{
+echo '<ul class="nav-child unstyled small">';
+}
+elseif ($item->shallower)
+{
+// The next item is shallower.
+echo '</li>';
+echo str_repeat('</ul></li>', $item->level_diff);
+}
+else
+{
+// The next item is on the same level.
+echo '</li>';
+}
+}
+?></ul>
 
